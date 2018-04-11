3049667be2f16edf767e36b0c10cabb51758d8e8
daniel-lundin <daniel.t.lundin@gmail.com>
2018-04-11 21:47:11 +0200

    bumped version

--- ui/main.js
+++ ui/main.js
@@ -1,37 +1,12 @@
 import { createChart } from "aboxd";
 
-function chart(str) {
-  // Before anyone sees this:
-  // I should probably contact the library author
-  // to make it return a string instead of writing
-  // to process.stdout and console.log
-  const oldLog = console.log;
-  let output = "";
-
-  window.process = {
-    stdout: {
-      write(val) {
-        output += val;
-      }
-    }
-  };
-  console.log = val => {
-    output += val + "\n";
-  };
-
-  createChart(str);
-  console.log = oldLog;
-
-  return output;
-}
-
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 
-output.innerText = chart(input.value || "");
+output.innerText = createChart(input.value || "");
 
 input.addEventListener("keyup", evt => {
-  output.innerText = chart(input.value || "");
+  output.innerText = createChart(input.value || "");
 });
 
 window.chart = chart;
